b35f0a848a23cdcde50e4ac6121d2839f278c59c
Cure-X <admin@cure-x.net>
2024-04-15 12:22:55 +0800

    commit

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -14,6 +14,28 @@ export const zhSidebar = sidebar({
       prefix: 'elements/',
       link: 'elements/', //？
       children: ['literal.md', 'identifier.md', 'keyword.md', 'relation.md', 'comment.md', 'escape.md', 'punc.md']
+    },
+    {
+      text: '语法与结构',
+      prefix: 'structure',
+      children: ['statement.md', 'dec_and_def.md', 'codeblock.md', 'condition.md', 'loop.md', 'function.md', '过程', 'namespace.md', 'class.md', 'enum.md']
+    },// 抽象层，作用域，生命周期
+    {
+      text: '（基本）数据类型',
+    },
+    {
+      text: '包管理（系统）'
+    },
+    {
+      text: '多范式',
+      children: [
+        {
+          text: '面向对象' // 泛型
+        }
+      ]
     }
   ],
 });
+// 基础算法
+
+// 其他文档
